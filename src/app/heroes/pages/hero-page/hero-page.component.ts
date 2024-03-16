2e1458c96a5ae7bc0910ff7e092d48ca36321647
Nuria Oujo <nuriaoujo.n1@gmail.com>
2024-03-16 18:38:47 +0100

    Hero page description started

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { delay, switchMap } from 'rxjs';
 import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
@@ -23,6 +23,7 @@ export class HeroPageComponent implements OnInit {
   ngOnInit(): void {
       this.activatedRoute.params
         .pipe(
+          delay(3000),
           switchMap( ({id}) => this.heroesService.getHeroById(id)),
         )
         .subscribe(hero => {
@@ -32,6 +33,10 @@ export class HeroPageComponent implements OnInit {
           console.log(hero);
           return;
         })
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
   }
 
 }
