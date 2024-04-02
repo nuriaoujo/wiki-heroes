a37a17c43200e5d665816604e1bfe565ff66d762
Nuria Oujo <nuriaoujo.n1@gmail.com>
2024-04-02 11:01:22 +0200

    types added to newpage component

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -9,13 +10,13 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class NewPageComponent {
 
   public heroForm = new FormGroup({
-    id: new FormControl(''),
-    superhero: new FormControl(''),
-    publisher: new FormControl(''),
-    alter_ego: new FormControl(''),
-    first_appearance: new FormControl(''),
-    characters: new FormControl(''),
-    alt_img: new FormControl('')
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego: new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img: new FormControl<string>('')
   })
 
   public publishers = [
