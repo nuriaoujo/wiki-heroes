4ac223eedb11c4ee26415d72abb0248dd1e13200
Nuria Oujo <nuriaoujo.n1@gmail.com>
2024-04-24 13:05:26 +0200

    authentication function created

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,34 +1,54 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, tap, of, map, catchError } from 'rxjs';
+
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
-    private baseUrl = environments.baseUrl;
-    private user?: User;
+  private baseUrl = environments.baseUrl;
+  private user?: User;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    get currentUser(): User | undefined {
-        if(!this.user) return undefined;
-        return structuredClone(this.user); // Para crear un clon profundo
-    }
+  get currentUser():User|undefined {
+    if ( !this.user ) return undefined;
+    return structuredClone( this.user );
+  }
 
-    login(email: string, password: string): Observable<User> {
-        
-        return this.http.get<User>(`${this.baseUrl}/users/1`)
-            .pipe(
-                tap( user => this.user = user),
-                tap( user => localStorage.setItem('token', 'udobusadbsd.dfsdgfsdgds.awrwete') ),
-            );
-    }
+  login( email: string, password: string ):Observable<User> {
+    // http.post('login',{ email, password });
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k' )),
+      );
+  }
 
-    logout() {
-        this.user = undefined;
-        localStorage.clear();
-    }
+  checkAuthentication(): Observable<boolean> {
+
+    if ( !localStorage.getItem('token') ) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      );
+
+  }
+
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+
 
 }
